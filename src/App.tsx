0c57cd288b4c165f51577e8fa1863dab6a7f6a86
makstravm <maks7-2-94@gmail.com>
2021-05-14 00:20:22 +0300

    added handler remove task, and filter task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v1 } from 'uuid';
 import './App.css';
 import { TodoList } from './TodoLIst';
 
+export type FilterType = 'all' | 'active' | 'completed'
+
 function App() {
+  const [task, setTask] = useState([
+    { id: v1(), title: 'Cabinet', isDone: true },
+    { id: v1(), title: 'Golden Ale', isDone: false },
+    { id: v1(), title: 'Silver Ale', isDone: false },
+    { id: v1(), title: 'Zhiguli', isDone: true },
+    { id: v1(), title: 'Mykulinckoe', isDone: true }
+  ])
+
+  function removeTask(taskId: string) {
+    const newTask = task.filter(t => t.id !== taskId)
+    setTask(newTask)
+  }
+  const [filter, setFilter] = useState('all')
+
+  function changeTodList(value: FilterType) {
+    setFilter(value)
+  }
+
+  function filterTasks() {
+    if (filter === 'active') {
+      return task.filter(t => t.isDone === false)
+    }
+    else if (filter === 'completed') {
+      return task.filter(t => t.isDone === true)
+    }
+    else {
+      return task
+    }
+  }
   return (
     <div className="App">
-      <TodoList />
+      <TodoList
+        title='Beer varieties'
+        tasks={filterTasks()}
+        removeTask={removeTask}
+        changeTodList={changeTodList}
+      />
     </div>
   );
 }
--- src/TodoLIst.tsx
+++ src/TodoLIst.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
+import { FilterType } from './App';
 import './App.css';
 
-export function TodoList() {
+type TaskType = {
+  id: string
+  title: string
+  isDone: boolean
+}
+type TodoListPropsType = {
+  title: string
+  tasks: Array<TaskType>
+  removeTask: (taskId: string) => void
+  changeTodList: (value: FilterType) => void
+}
+
+export function TodoList(props: TodoListPropsType) {
+  const todoListTask = props.tasks.map(t => {
+    const removeTaskHandler = () => props.removeTask(t.id)
+    return (
+      <li>
+        <input type="checkbox" checked={t.isDone} />
+        <span>{t.title}</span>
+        <button onClick={removeTaskHandler}>X</button>
+      </li>
+    )
+  })
+  const filterTodoListAll = () => props.changeTodList('all');
+  const filterTodoListActive = () => props.changeTodList('active');
+  const filterTodoListCompleted = () => props.changeTodList('completed');
   return (
     <div>
-      <h3>What to learn</h3>
+      <h3>{props.title}</h3>
       <div>
         <input />
         <button>+</button>
       </div>
       <ul>
-        <li><input type="checkbox" checked={true} /> <span>HTML&CSS</span></li>
-        <li><input type="checkbox" checked={true} /> <span>JS</span></li>
-        <li><input type="checkbox" checked={false} /> <span>React</span></li>
+        {todoListTask}
       </ul>
       <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button onClick={filterTodoListAll}>All</button>
+        <button onClick={filterTodoListActive}>Active</button>
+        <button onClick={filterTodoListCompleted}>Completed</button>
       </div>
     </div>
   )
