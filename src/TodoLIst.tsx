19d2124ca1ba7d990ef7b4c5ed11299d09eb398f
makstravm <maks7-2-94@gmail.com>
2021-05-31 19:16:10 +0300

    added many todolists, delete todolist onClick

--- src/TodoLIst.tsx
+++ src/TodoLIst.tsx
@@ -10,21 +10,24 @@ export type TaskType = {
 type TodoListPropsType = {
   title: string
   tasks: Array<TaskType>
-  removeTask: (taskId: string) => void
-  changeTodList: (value: FilterType) => void
-  addTask: (title: string) => void
-  checkPropsStatus: (taskId: string, isDone: boolean) => void
-  filter: any
+  removeTask: (taskId: string, todoListId: string) => void
+  changeTodList: (value: FilterType, todoListId: string) => void
+  addTask: (title: string, todoListId: string) => void
+  checkPropsStatus: (taskId: string, isDone: boolean, todoListId: string) => void
+  filter: FilterType
+  id: string
+  removeTodoList: (todoListId: string) => void
 }
 
 export function TodoList(props: TodoListPropsType) {
-  const [newTitle, setNewTitle] = useState('')
-  const [error, setError] = useState(false)
+  const [newTitle, setNewTitle] = useState('') // локальный стейт для заполнения инпуту
+  const [error, setError] = useState(false) //  лоакльный стейт для контроля ошибки в инпуте
   const todoListTask = props.tasks.map(t => {
-    const removeTaskHandler = () => props.removeTask(t.id)
-    const changeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => props.checkPropsStatus(t.id, e.currentTarget.checked);
+    const removeTaskHandler = () => props.removeTask(t.id, props.id) // удаления таски
+    const changeStatusHandler = (e: ChangeEvent<HTMLInputElement>) => props.checkPropsStatus(t.id, e.currentTarget.checked, props.id); //  отслеживание чекбокса статуса 
     return (
       <li key={t.id} className={s.item}>
+        {/* key - используем всегда в Реакт при Map */}
         <label>
           <input
             className={s.checkbox}
@@ -38,30 +41,34 @@ export function TodoList(props: TodoListPropsType) {
       </li>
     )
   })
-  const filterTodoListAll = () => props.changeTodList('all');
-  const filterTodoListActive = () => props.changeTodList('active');
-  const filterTodoListCompleted = () => props.changeTodList('completed');
+  const filterTodoListAll = () => props.changeTodList('all', props.id);
+  const filterTodoListActive = () => props.changeTodList('active', props.id);
+  const filterTodoListCompleted = () => props.changeTodList('completed', props.id);
   const classFilterAll = (props.filter === 'all') && s.active
   const classFilterActive = (props.filter === 'active') && s.active
   const classFilterComleted = (props.filter === 'completed') && s.active
   const addTaskHandler = () => {
-    if (newTitle.trim() !== '') {
-      props.addTask(newTitle.trim())
+    if (newTitle.trim() !== '') { //метод удаляет лишнии пробелы
+      props.addTask(newTitle.trim(), props.id)
       setNewTitle('')
     } else {
       setError(true)
     }
   }
-  const onChangeTask = (e: React.ChangeEvent<HTMLInputElement>) => setNewTitle(e.currentTarget.value)
+  const onChangeTask = (e: React.ChangeEvent<HTMLInputElement>) => setNewTitle(e.currentTarget.value)// отслеживание данных в инпуте
   const onPressKeyAddTask = (e: React.KeyboardEvent<HTMLInputElement>) => {
     setError(false)
     if (e.key === 'Enter') {
       addTaskHandler()
     }
   }
+  const removeTodoList = () => { props.removeTodoList(props.id) }
   return (
     <div>
       <h3>{props.title}</h3>
+      <button
+        onClick={removeTodoList}
+        className={s.delete}>delete</button>
       <div>
         <input value={newTitle}
           onChange={onChangeTask}
